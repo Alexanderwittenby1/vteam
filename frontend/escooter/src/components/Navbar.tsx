f3e8222ac93d6408c98f6886105ca66a10c61274
Alexander <allewi@live.se>
2024-11-28 14:05:45 +0100

    fungerar

---
+++ frontend/escooter/src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav style={{ padding: "10px", backgroundColor: "#333", color: "#fff" }}>
+      <ul style={{ display: "flex", gap: "15px", listStyleType: "none", margin: 0, padding: 0 }}>
+        <li>
+          <Link href="/" style={{ color: "#fff", textDecoration: "none" }}>Home</Link>
+        </li>
+        <li>
+          <Link href="/sum" style={{ color: "#fff", textDecoration: "none" }}>Sum</Link>
+        </li>
+        {/* Lägg till fler länkar här om du behöver */}
+      </ul>
+    </nav>
+  );
+}
