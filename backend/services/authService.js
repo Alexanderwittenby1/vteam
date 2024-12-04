176c9d43827e25f17d2c9263cba19534a137ae5f
Alexander <allewi@live.se>
2024-12-04 12:37:53 +0100

    scooters fungerar att hämta i frontend

---
+++ backend/services/authService.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const generateToken = (user) => {
+    return jwt.sign(
+        { id: user.id, email: user.email },
+        process.env.JWT_SECRET,
+        { expiresIn: '1h' }
+    );
+};
+
+module.exports = {
+    generateToken,
+};
