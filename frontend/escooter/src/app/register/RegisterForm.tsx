c4fd419b0564f0d1205ca1d592ef771460faf294
AlfredoSaucee <almquist.alfred@gmail.com>
2024-12-04 16:19:06 +0100

    fix

--- frontend/escooter/src/app/register/RegisterForm.tsx
+++ frontend/escooter/src/app/register/RegisterForm.tsx
@@ -5,34 +5,40 @@ function RegisterForm() {
   const [password, setPassword] = useState<string>("");
 
   const handleSubmit = async (event: React.FormEvent) => {
-    event.preventDefault(); // Prevent the default form submission behavior
-
     const userData = {
       email,
       password,
     };
 
     try {
-      const response = await fetch("http://vteam-backend-1/user/register", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(userData),
-      });
+      const response = await fetch(
+        "http://vteam-backend-1:4000/user/register",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(userData),
+        }
+      );
 
       if (response.ok) {
         console.log("User added successfully");
       } else {
         console.error("Failed to add user");
+        console.log(JSON.stringify(userData));
       }
-    } catch (error) {
-      console.error("Error submitting form:", error);
+    } catch (error: unknown) {
+      if (isErrorWithMessage(error)) {
+        console.error("Error submitting form:", error.message);
+      } else {
+        console.error("An unknown error occurred");
+      }
     }
   };
 
   return (
-    <section className="vh-100 ">
+    <section className="vh-100">
       <div className="mask d-flex align-items-center h-100 bg-color-1">
         <div className="container h-100">
           <div className="row d-flex justify-content-center align-items-center h-100">
@@ -51,7 +57,7 @@ function RegisterForm() {
                         onChange={(e) => setEmail(e.target.value)}
                         required
                         className="form-control form-control-lg"
-                      ></input>
+                      />
                       <label className="form-label" htmlFor="registerformemail">
                         Email
                       </label>
@@ -60,13 +66,16 @@ function RegisterForm() {
                     <div data-mdb-input-init className="form-outline mb-4">
                       <input
                         type="password"
-                        id="registerformemail"
+                        id="registerformpassword"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                         required
                         className="form-control form-control-lg"
-                      ></input>
-                      <label className="form-label" htmlFor="registerformemail">
+                      />
+                      <label
+                        className="form-label"
+                        htmlFor="registerformpassword"
+                      >
                         Password
                       </label>
                     </div>
@@ -76,7 +85,7 @@ function RegisterForm() {
                     </button>
 
                     <p className="text-center">
-                      Dont have an account?{" "}
+                      Don't have an account?{" "}
                       <a href="/register" className="fw-bold accent-color-1">
                         Register here
                       </a>
@@ -90,6 +99,11 @@ function RegisterForm() {
       </div>
     </section>
   );
+}
+
+// Type guard to check if the error is an instance of Error and has a message
+function isErrorWithMessage(error: unknown): error is Error {
+  return error instanceof Error && "message" in error;
 }
 
 export default RegisterForm;
