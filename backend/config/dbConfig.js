f95636f34858b7f58daf05373d6a694b716962d2
Alexander <allewi@live.se>
2024-12-04 16:38:47 +0100

    dsad

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST || 'db',
     port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER || 'yourUsername',
-    password: process.env.DB_PASSWORD || 'yourPassword',
+    password: process.env.DB_PASSWORD || 'P@ssw0rd',
     database: process.env.DB_NAME || 'scooter_sharing'
 });
 
