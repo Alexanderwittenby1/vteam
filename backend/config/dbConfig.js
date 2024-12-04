12cdff11a6bd0f26204486dd56536080ffa8b2e8
AlfredoSaucee <almquist.alfred@gmail.com>
2024-12-04 16:19:06 +0100

    fix

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -1,11 +1,11 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 3306,
-    user: process.env.DB_USER || 'yourUsername',
-    password: process.env.DB_PASSWORD || 'yourPassword',
-    database: process.env.DB_NAME || 'yourDatabaseName'
+  host: process.env.DB_HOST || "db",
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER || "adabm",
+  password: process.env.DB_PASSWORD || "P@ssw0rd",
+  database: process.env.DB_NAME || "scooter_sharing",
 });
 
 module.exports = pool;
