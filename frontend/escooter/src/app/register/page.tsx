b35242695e43ef36f935367dbf8445f0f0879e02
AlfredoSaucee <almquist.alfred@gmail.com>
2024-12-04 13:53:17 +0100

    bootstrap module install

--- frontend/escooter/src/app/register/page.tsx
+++ frontend/escooter/src/app/register/page.tsx
@@ -1,16 +1,13 @@
-"use client"
+"use client";
 import Navbar from "../../components/Navbar"; // Importera Navbar-komponenten
-
-
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function register() {
   return (
     <div>
       <Navbar />
       <h1>Register</h1>
-      <p>Here you can see a list of scooters</p>
-      
+      <p>Hej</p>
     </div>
   );
-  
 }
