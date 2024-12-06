0fd90b948cf9c95ad69d3d70ce2639d95a89b4b7
AlfredoSaucee <almquist.alfred@gmail.com>
2024-12-06 14:36:39 +0100

    sidebar

--- frontend/escooter/src/app/scooters/page.tsx
+++ frontend/escooter/src/app/scooters/page.tsx
@@ -1,16 +1,15 @@
-"use client"
+"use client";
+import Sidebar from "@/components/Sidebar";
 import Navbar from "../../components/Navbar"; // Importera Navbar-komponenten
 import ScooterList from "../../components/scooterData/scooterlist"; // Importera ScooterList-komponenten
-
 
 export default function scooters() {
   return (
     <div>
-      <Navbar />
+      <Sidebar />
       <h1>Welcome to the scooters page</h1>
       <p>Here you can see a list of scooters</p>
       <ScooterList />
     </div>
   );
-  
 }
