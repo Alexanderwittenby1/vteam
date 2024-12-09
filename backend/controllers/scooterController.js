fbe9e72fd476afd5248df06cb762cca2c42f88da
Alexander <allewi@live.se>
2024-12-09 13:54:03 +0100

    added

--- backend/controllers/scooterController.js
+++ backend/controllers/scooterController.js
@@ -1,10 +1,10 @@
-const scootermodel = require('../models/scooterModel');
+const scootermodel = require("../models/scooterModel");
 
 exports.getAll = (req, res) => {
-    scootermodel.getAllScootersFromdb((error, scooters) => {
-        if (error) {
-            return res.status(500).json({ error: 'Internt serverfel' });
-        }
-        res.status(200).json(scooters);
-    });
-}
+  scootermodel.getAllScootersFromdb((error, scooters) => {
+    if (error) {
+      return res.status(500).json({ error: "Internt serverfel" });
+    }
+    res.status(200).json(scooters);
+  });
+};
--- backend/routes/scooterRoutes.js
+++ backend/routes/scooterRoutes.js
@@ -1,8 +1,7 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const scooterController = require('../controllers/scooterController');
+const scooterController = require("../controllers/scooterController");
 
-
-router.get('/all', scooterController.getAll);
+router.get("/all", scooterController.getAll);
 
 module.exports = router;
