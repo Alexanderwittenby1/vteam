6c32e6976d6c9b48e10184df2ced4a0e14c1b1c5
hassanabdulmouti <haad23@student.bth.se>
2024-12-13 10:31:12 +0100

    burger menu

--- frontend/escooter/src/app/login/page.tsx
+++ frontend/escooter/src/app/login/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 import RegisterForm from "./LoginForm";
-
+import Navbar from '@/components/Navbar';
 import "bootstrap/dist/css/bootstrap.css";
 
 export default function register() {
   return (
     <div>
+      <Navbar />
+    <div>
       <RegisterForm />
+    </div>
     </div>
   );
 }
